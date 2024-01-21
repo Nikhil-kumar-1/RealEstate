fdb138e0fa81a2f0d9d31e4503cf09d447b550b6
Nikhil <kumarnikhil787031@gmail.com>
2024-01-21 19:45:34 +0530

    frontend page design

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,21 @@
 const express = require("express");
-const { createUser } = require("../controllers/userCntrl");
+const {
+  createUser,
+  bookVisit,
+  getAllBookings,
+  cancelBooking,
+  toFav,
+  getAllFavorites,
+} = require("../controllers/userCntrl");
 
 const router = express.Router();
 
+// Use router.post() for routes that handle POST requests
 router.post("/register", createUser);
+router.post("/bookVisit/:id", bookVisit);
+router.post("/allBookings", getAllBookings);
+router.post("/removeBooking/:id", cancelBooking);
+router.post("/toFav/:rid", toFav);
+router.post("/allFav/", getAllFavorites);
 
 module.exports = { userRoute: router };
